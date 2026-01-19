1ba731f89ecec334a8d19a6b72cf031a7aaddc3c
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-18 18:15:59 -0600

    Trying to setup pages

---
+++ public/js/import.js
@@ -0,0 +1,13 @@
+// Note to self: jQuery .load() function takes an absolute path from the project root.
+$(function ()
+{
+    $("#navbar").load("navbar.html");
+});
+$(function ()
+{
+    $("#footer").load("footer.html");
+});
+$(function ()
+{
+    $("#footer-a").load("footer-abbreviated.html");
+});
