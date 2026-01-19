db7fda1b05219c06eb70fe9622ac4014aa056398
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-19 17:37:29 -0600

    Home Page Finished!

--- public/js/import.js
+++ public/js/import.js
@@ -1,13 +1,21 @@
 // Note to self: jQuery .load() function takes an absolute path from the project root.
-$(function ()
-{
-    $("#navbar").load("navbar.html");
-});
-$(function ()
-{
-    $("#footer").load("footer.html");
-});
-$(function ()
-{
-    $("#footer-a").load("footer-a.html");
-});
+// $(function ()
+// {
+//     $("#navbar").load("navbar.html");
+// });
+// $(function ()
+// {
+//     $("#footer").load("footer.html");
+// });
+// $(function ()
+// {
+//     $("#footer-a").load("footer-a.html");
+// });
+
+$(function(){
+    var imports = $('[data-import]');
+    $.each(imports, function() {
+        var file = "imports/" + $(this).data('import') + ".html";
+        $(this).load(file);
+    })
+})
